a0fa52bf15aca3bc3b5a19c63f6fe71c2540a544
josefpomikalek <josef_pomikalek@yahoo.com>
2024-11-03 20:42:48 +0100

    Modify the output of function time_to_seconds.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def time_to_seconds(time):
     minutes = int(time[:2])
     seconds = int(time[3:])
     time_in_seconds = minutes * 60 + seconds
-    print(time_in_seconds)
+    return time_in_seconds
 
 
 time_entered = input("Enter your time (use format 'h:mm:ss' or 'mm:ss'): ")
 print(time_entered)
-time_to_seconds(time_entered)
+print(time_to_seconds(time_entered))

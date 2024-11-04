7aeb58bbfbf42c714ffbb32e370b30b5f1c680dd
josefpomikalek <josef_pomikalek@yahoo.com>
2024-11-04 20:45:57 +0100

    Complete the function time_to_seconds.

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 def time_to_seconds(time):
 
     """Transfer the time entered as a string to seconds"""
-    minutes = int(time[:2])
-    seconds = int(time[3:])
-    time_in_seconds = minutes * 60 + seconds
+    if len(time) == 8:  # For the time format hh:mm:ss
+        hours = int(time[:2])
+        minutes = int(time[3:5])
+        seconds = int(time[6:])
+    elif len(time) == 7:    # For the time format h:mm:ss
+        hours = int(time[:1])
+        minutes = int(time[2:4])
+        seconds = int(time[5:])
+    elif len(time) == 4:    # For the time format mm:ss
+        hours = 0
+        minutes = int(time[:1])
+        seconds = int(time[2:])
+    else:   # For the time format m:ss
+        hours = 0
+        minutes = int(time[:2])
+        seconds = int(time[3:])
+    time_in_seconds = hours * 3600 + minutes * 60 + seconds
     return time_in_seconds
 
 
@@ -12,5 +26,3 @@ time_entered = input("Enter your time (use format 'h:mm:ss' or 'mm:ss'): ")
 print(time_entered)
 print(time_to_seconds(time_entered))
 
-# the possibility for m:ss format, e.g. 3:55
-# the options for 'hours' and 'm:ss'

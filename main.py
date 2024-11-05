6f32b883c63f52b4bed0c40095dd009f8b7ea122
josefpomikalek <josef_pomikalek@yahoo.com>
2024-11-05 22:36:21 +0100

    Get the raw output from the function seconds_to_time.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import floor
 
 def time_to_seconds(time):
 
@@ -24,8 +24,13 @@ def time_to_seconds(time):
 
 
 """Transfer the time entered in seconds to time format."""
-def seconds_to_time(seconds):
-    pass
+def seconds_to_time(seconds_count):
+    hours = seconds_count // 3600
+    minutes = floor((seconds_count - (hours * 3600)) / 60)
+    seconds = seconds_count - (hours * 3600 + minutes * 60)
+    print(hours)
+    print(minutes)
+    print(seconds)
 
 
 def calculate_pace(time, distance):
@@ -33,10 +38,11 @@ def calculate_pace(time, distance):
     return pace
 
 
-distance_entered = input("Enter the distance in kilometres: ")
-time_entered = input("Enter your time (use format 'h:mm:ss' or 'mm:ss'): ")
-print(time_entered)
-print(time_to_seconds(time_entered))
+# distance_entered = input("Enter the distance in kilometres: ")
+# time_entered = input("Enter your time (use format 'h:mm:ss' or 'mm:ss'): ")
+# print(time_entered)
+# print(time_to_seconds(time_entered))
+#
+# print(calculate_pace(time_to_seconds(time_entered), distance_entered))
 
-print(calculate_pace(time_to_seconds(time_entered), distance_entered))
-
+seconds_to_time(4128)
